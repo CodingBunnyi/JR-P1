cb85dafe5cecb716bb7b1573f7cde908c828cc85
YUHAN QIAN <yh984980263@gmail.com>
2021-05-18 16:58:14 +0800

    react start

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-react']
+              }
+            }
+        }]
+    }  
+  }
+  
