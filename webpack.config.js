3f31a129feca56e43dbdcf0776d3aab7472d9fc5
YUHAN QIAN <yh984980263@gmail.com>
2021-05-24 02:21:24 +0800

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode:'development',
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
   module: {
     rules: [
       {
@@ -13,5 +21,21 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  devServer: {
+    contentBase: './',
+    open: true,
+    port: 9000,
+    publicPath: './',
+    watchContentBase: true,
+    hot: true,
+    inline: true
+  },
+
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: './src/index.html',
+  //   }),
+  //   new CleanWebpackPlugin()
+  // ]
 };
